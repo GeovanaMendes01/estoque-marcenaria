cb88fb18c374f0a198945b2032ded6ced1b1cb86
GuilhermeJT <guilherej.2309@gmail.com>
2026-06-10 21:10:42 -0400

    relatorio xslx - baixas

--- progWeb/src/main/java/br/ufms/facom/progWeb/repositories/BaixaRepository.java
+++ progWeb/src/main/java/br/ufms/facom/progWeb/repositories/BaixaRepository.java
@@ -3,5 +3,14 @@ package br.ufms.facom.progWeb.repositories;
 import br.ufms.facom.progWeb.models.Baixa;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface BaixaRepository extends JpaRepository<Baixa, Long> {
+
+    List<Baixa> findByDataBetween(LocalDate inicio, LocalDate fim);
+
+    List<Baixa> findByDataGreaterThanEqual(LocalDate inicio);
+
+    List<Baixa> findByDataLessThanEqual(LocalDate fim);
 }
